c018db8ef10327cfa29c18b66c8dc7dacd4a8abc
hamim-nibir <nhnibir.te.com>
2024-11-27 09:48:06 +0600

    nothing

--- updateProfile.php
+++ updateProfile.php
@@ -5,7 +5,8 @@ if (isset($_SESSION['user_id']) && isset($_SESSION['email'])) {
     $user_id = $_SESSION['user_id'];
     $email = $_SESSION['email'];
 
-    header('Location: dashboard.html');
+    echo "Welcome, User ID: " . $user_id . "<br>";
+    echo "Your Email: " . $email;
 } else {
     echo "Please log in to access this page.";
     header("Location: loginRegister.html");
